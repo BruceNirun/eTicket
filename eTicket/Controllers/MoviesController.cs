a45343183e99e70ad1eeba4e209b546dec91b0e9
BruceNirun19 <nirun.h@planetcomm.com>
2023-05-31 23:56:16 +0700

    1e

--- eTicket/Controllers/MoviesController.cs
+++ eTicket/Controllers/MoviesController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTicket.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbcontext _context;
+
+        public MoviesController(AppDbcontext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovie = await _context.Movies.ToListAsync();
             return View();
         }
     }
